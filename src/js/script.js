e89f52ee3d97ff0c5f902344d4971bf450b4d02a
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2024-04-21 17:46:03 -0300

    ..
    adicionando novas coisas

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 function removerEspacos() {
-    let textoOriginal = document.getElementById("textoOriginal").value;
-    let textoSemEspacos = textoOriginal.replace(/\s/g, "");
-    document.getElementById("resultado").innerHTML = textoSemEspacos;
+    let textoOriginal = document.getElementById("textoOriginal").value
+    let textoSemEspacos = textoOriginal.replace(/\s/g, "")
+    document.getElementById("resultado").innerHTML = textoSemEspacos
+}
+
+const input = document.getElementById("resutado")
+
+function copybutton() {
+  navigator.clipboard.writeText(input.value).then(() => {
+    alert('Copied to Clipboard')
+  })
 }
