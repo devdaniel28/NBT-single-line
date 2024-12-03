3a456252c6b750c16a828d223ad96b7aa6a14c68
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2024-12-03 13:59:49 -0300

    Ajuste no codigo

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 function removerEspacos() {
     let nbtClean = document.getElementById("nbtClean").value
     let nbtSingleLine = nbtClean.replace(/\s/g, "")
-    document.getElementById("resultado").innerHTML = nbtSingleLine
+    document.getElementById("resultado").innerText = nbtSingleLine
 } // o que faz a magica aconteçer kk
 
 async function copiarNbt() {
@@ -11,7 +11,7 @@ async function copiarNbt() {
   try {
     await navigator.clipboard.writeText(nbt.textContent);
     console.log('Texto copiado para a área de transferência');
-    alert("Nbt Copiado!")
+    nbt.innerText = 'Nbt Copiado!'
   } catch (err) {
     console.error(err);
   }
