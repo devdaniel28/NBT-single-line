95cd68f3d2efa0b7477cb5f3e81cc1cf40d64572
devDaniel28 <113447778+devdaniel28@users.noreply.github.com>
2025-11-04 16:46:11 -0300

    Ajuste e melhorias no Estilo do site

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,16 @@ function removerEspacos() {
     let nbtClean = document.getElementById("nbtClean").value
     let nbtSingleLine = nbtClean.replace(/\s/g, "")
     document.getElementById("resultado").innerText = nbtSingleLine
-} // o que faz a magica aconteçer kk
+}
 
+//* Função de Ctrl + C
 async function copiarNbt() {
-  
-  var nbt = document.getElementById("resultado");
-
+  let nbt = document.getElementById("resultado")
   try {
-    await navigator.clipboard.writeText(nbt.textContent);
-    console.log('Texto copiado para a área de transferência');
+    await navigator.clipboard.writeText(nbt.textContent)
+    console.log('Texto copiado para a área de transferência')
     nbt.innerText = 'Nbt Copiado!'
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
 }
